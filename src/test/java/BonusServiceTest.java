e6151c842ff17743ef23302f378fcd3cb1fdfe45
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2022-05-24 21:24:10 +0300

    CsvFileSource added

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void test(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+}
